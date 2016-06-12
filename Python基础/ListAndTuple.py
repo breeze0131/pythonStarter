90702bd7ea168f6ea15f7f3ae0a25960e23e212c
ChasonWang <378161085@qq.com>
2016-06-12 23:44:01 +0800

    List and Tuple

---
+++ Python基础/ListAndTuple.py
@@ -0,0 +1,14 @@
+family=['father','mother','son']
+print('the old man is',family[0])
+family.insert(3,'sister')
+print(family[3])
+family.append('brother')
+print(family[-1])
+print(len(family))
+family.pop()
+print(len(family))
+
+
+t=('abc',)
+print(family)
+print(t)
