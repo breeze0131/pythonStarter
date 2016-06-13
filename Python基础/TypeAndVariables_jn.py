e5bb0c7d5d1f06b09480487a0d069f653c28bb9f
breeze0131 <breeze@symbi.in>
2016-06-13 20:57:58 +0800

    类型与变量

---
+++ Python基础/TypeAndVariables_jn.py
@@ -0,0 +1,54 @@
+# 整数
+1
+-9900
+# 16进制整数
+0x111222
+# 浮点数
+1.23
+-9.01
+# 字符串
+'abc'
+"abc"
+"I'm ok"
+'I\'m ok'
+'\n\\'
+# r开头不转译
+r'\\\\'
+# 多行文本
+'''line1
+    line2
+    line3'''
+# 布尔
+True and False
+# 空值
+None
+
+# 变量
+a = 1
+t_007 = 'T007'
+
+# 常量
+PI = 3.14
+
+# 练习
+# 打印结果
+# 123
+# 456.789
+# Hello, world
+# Hello, 'Adam'
+# Hello, "Bart"
+# Hello,
+# Lisa!
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
