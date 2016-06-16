ce5c0db037a553d413320b1fd9051accefefb9b6
breeze0131 <breeze@symbi.in>
2016-06-16 23:05:55 +0800

    调用函数

---
+++ 函数/FunctionCall_jn.py
@@ -0,0 +1,26 @@
+# 查看帮助
+help(abs)
+
+# abs max
+abs(-1)
+max(1, 2, 3, 4)
+
+# 数据类型转换
+int('123')
+float('123.1')
+bool('1')
+str(123)
+
+# 函数别名？
+a = abs
+a(-1)
+
+# 练习
+#
+# 请利用Python内置的hex()函数把一个整数转换成十六进制表示的字符串：
+
+
+n1 = 255
+n2 = 1000
+print(str(hex(n1)))
+print(str(hex(n2)))
