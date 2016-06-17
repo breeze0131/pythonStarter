4c74d2ef1d86f2a63340cdbc62ec952bcf88ffc8
breeze0131 <breeze@symbi.in>
2016-06-17 23:34:04 +0800

    函数递归

--- 函数/RecursiveFunction_jn.py
+++ 函数/RecursiveFunction_jn.py
@@ -13,6 +13,7 @@ def tail_fact(n, product=1):
     else:
         return tail_fact(n - 1, product * n)
 
+
 print(tail_fact(3))
 
 
@@ -21,5 +22,13 @@ print(tail_fact(3))
 # 汉诺塔的移动可以用递归函数非常简单地实现。
 #
 # 请编写move(n, a, b, c)函数，它接收参数n，表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法，例如：
-# def move(n, a, b, c):
-    
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '->', c)
+    else:
+        move(n - 1, a, c, b)
+        move(1, a, b, c)
+        move(n - 1, b, a, c)
+
+
+move(3, 'A', 'B', 'C')
